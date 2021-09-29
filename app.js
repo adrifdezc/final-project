aa17122c36758b3601c7bceb298f89cb36e5c2a6
Adrian Fernandez Carmona <adrianfdezbcn@gmail.com>
2021-09-29 11:15:27 +0200

    env

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ const taskRouter = require("./routes/task.routes");
 app.use("/api",isAuthenticated, taskRouter);            // <== UPDATE
 
 
-app.use((req, res, next) => {
-    // If no routes match, send them the React HTML.
-    res.sendFile(__dirname + "/public/index.html");
-  });
+// app.use((req, res, next) => {
+//     // If no routes match, send them the React HTML.
+//     res.sendFile(__dirname + "/public/index.html");
+//   });
 
-// require("./error-handling")(app);
+require("./error-handling")(app);
 
 module.exports = app;
